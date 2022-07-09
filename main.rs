e85210872ccb0de56830d4f546d8e4c40ba4ef48
Caleb Kugel <calebkugel1@gmail.com>
2022-07-08 22:48:03 -0500

    init commit 2

---
+++ main.rs
@@ -0,0 +1,17 @@
+//Declare dependencies
+use std::io::stdin;
+
+fn writeAndHash(String& start) {
+    
+}
+
+fn main() {
+    let mut start_folder : String = String::new();
+
+    println!("Enter a start folder: ");
+    
+    stdin().read_line(&mut start_folder).ok();
+
+    
+
+}
