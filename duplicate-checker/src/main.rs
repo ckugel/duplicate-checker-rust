70c771c983a8c63e0a2b5d52d9d7150f14bac40f
Caleb Kugel <calebkugel1@gmail.com>
2022-07-12 14:33:05 -0500

    works for PNG

--- duplicate-checker/src/main.rs
+++ duplicate-checker/src/main.rs
@@ -1,9 +1,12 @@
 extern crate image;
 extern crate img_hash;
 
+use img_hash::image::EncodableLayout;
 use img_hash::{HasherConfig, HashAlg};
 
-use std::collections::hash_map::HashMap;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::hash_set::HashSet;
+use std::hash::{Hash, Hasher};
 
 use sha2::Sha512;
 use sha2::Digest;
@@ -14,24 +17,69 @@ use std::vec::Vec;
 use std::fs::File;
 use std::io::prelude::*;
 
-use std::fs;
+use std::{fs, hash};
 use std::env;
 
 use glob::glob;
 use std::path::PathBuf;
 
+use sha2::Sha256;
+use std::io;
+
+use std::io::BufReader;
+
+use string_builder::Builder;
+
+use base64ct::{Base64, Encoding};
+
+
 fn viewPNG(start : &str) -> () {
-    let mut map = HashMap::new();
-    let hasher = HasherConfig::new().to_hasher();
+    let mut set : HashSet<Vec<u8>>  = HashSet::new();
     for entry in glob(&(format!("{}/**/*.png", start)) as &str).expect("Failed to read glob pattern") {
 
-        let hashedValue = Sha512::digest(&entry.unwrap().into_os_string().into_string().unwrap();
+        // let mut hasher = Sha256::new();
+        // let mut file = fs::File::open(&entry.unwrap().as_path()).unwrap();
 
-        if (map.contains_key(&hashedValue)) {
-            
+        // let bytes_written = io::copy(&mut file, &mut hasher);
+        // let hash_bytes = hasher.finalize();
+
+        let path = &entry.unwrap();
+
+        let file: File = File::open(path).expect("Failed to read line");
+        let mut reader: BufReader<File>  = BufReader::new(file);
+        let mut buffer: Vec<u8> = Vec::new();
+
+        reader.read_to_end(&mut buffer);
+
+        let mut hasher : Sha512 = Sha512::new();
+        hasher.update(&buffer);
+        let vec : Vec<u8> = hasher.finalize().to_vec();
+
+        if set.contains(&vec) {
+            std::fs::remove_file(path).ok();
+        }
+        else {
+            set.insert(vec);
         }
 
-        println!("{:?}", entry.unwrap().display());
+        
+
+        // let mut builder = Builder::new(64);
+
+        // let hashedValue = Sha512::digest(&entry.unwrap().into_os_string().into_string().unwrap());
+
+        // for item in hashedValue {
+        //     builder.append(char::from(item));
+        // }
+
+        // println!("Vector: {:?} \n", hashedValue);
+        // println!("{} \n\n\n", builder.string().unwrap());
+
+        // if (map.contains_key(&hashedValue)) {
+            
+        // }
+
+        // println!("{:?}", entry.unwrap().display());
     }
 }
 
@@ -60,6 +108,20 @@ fn main() -> std::io::Result<()> {
     // println!("Contents: {}", data);
 
     viewPNG(&start_folder as &str);
+
+
+    // let vec : Vec<u8> = vec![65, 34, 127, 99, 2, 34];
+    // let vec2 : Vec<u8> = vec![65, 34, 127, 99, 2, 34];
+
+    // let mut hash = DefaultHasher::new();
+    // let mut hash2 = DefaultHasher::new();
+
+    // vec.hash(&mut hash);
+    // vec2.hash(&mut hash2);
+
+    // println!("hash 1: {:?}", hash.finish());
+    // println!("hash 2: {:?}", hash2.finish());
+    
 
     Ok(())
 } 
