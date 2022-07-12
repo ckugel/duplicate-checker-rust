2b92ccb3e5a17d240f5858c6997831375baaf0f2
Caleb Kugel <calebkugel1@gmail.com>
2022-07-11 23:06:26 -0500

    I am pissed

--- duplicate-checker/src/main.rs
+++ duplicate-checker/src/main.rs
@@ -1,7 +1,9 @@
 extern crate image;
 extern crate img_hash;
 
-use img_hash::{ImageHash, HashType};
+use img_hash::{HasherConfig, HashAlg};
+
+use std::collections::hash_map::HashMap;
 
 use std::io::stdin;
 use std::vec::Vec;
@@ -12,23 +14,25 @@ use std::io::prelude::*;
 use std::fs;
 use std::env;
 
-fn writeAndHash(start : String) {
-    if let Ok(entries) = fs::read_dir(start) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                // Here, `entry` is a `DirEntry`.
-                if let Ok(file_type) = entry.file_type() {
-                    // Now let's show our entry's file type!
-                    println!("{:?}: {:?}", entry.path(), file_type);
-                } else {
-                    println!("Couldn't get file type for {:?}", entry.path());
-                }
-            }
+use glob::glob;
+use std::path::PathBuf;
+
+fn viewPNG(start : &str) -> () {
+    let mut map : HashMap<img_hash::ImageHash, PathBuf> = HashMap::new();
+    let hasher = HasherConfig::new().to_hasher();
+    for entry in glob(&(format!("{}/**/*.png", start)) as &str).expect("Failed to read glob pattern") {
+
+        let image1 = image::open(&entry.unwrap().as_path()).unwrap();
+
+        let hashedValue : img_hash::ImageHash = hasher.hash_image(&image1);
+        if (map.contains_key(&hashedValue)) {
+            
         }
+        println!("{:?}", entry.unwrap().display());
     }
 }
 
-/*
+
 fn main() -> std::io::Result<()> {
     let mut start_folder : String = String::new();
 
@@ -40,26 +44,48 @@ fn main() -> std::io::Result<()> {
     // start_folder.insert_str(0, &file);
     // fs::canonicalize(&start_folder);
 
-    println!("Folder: {}", start_folder);
+    start_folder.truncate(start_folder.len() - 1);
 
-    let mut file = File::open(start_folder).expect("File not found");
+    // println!("Folder: {}", start_folder);
 
-    let mut data : String = String::new();
+    // let mut file = File::open(&start_folder as &str).expect("File not found");
 
-    file.read_to_string(&mut data).expect("Something went wrong reading the file");
+    // let mut data : String = String::new();
 
-    println!("Contents: {}", data);
+    // file.read_to_string(&mut data).expect("Something went wrong reading the file");
+
+    // println!("Contents: {}", data);
+
+    viewPNG(&start_folder as &str);
 
     Ok(())
 } 
-*/
 
+
+    /*
 fn main() {
     // let mut file : File = File::create("hello.txt").expect("Error encountered while creating file!");
     // file.write_all(b"what in the son of a yak").expect("Error while writing to file");
-    let mut thing : String = String::from("bruh.txt");
-    // (&thing as &str).what_is_this();
-    let file1 = File::open(&thing as &str).expect("File not found");
-    let file2 = File::open("bruh.txt").expect("File not found");
+
+    let mut thing : String = String::new();
+    
+    stdin().read_line(&mut thing).expect("Failed to read line");
+
+    thing.truncate(thing.len() - 1);
+
+    println!("{}", thing);
+    println!("bruh.txt");
+    assert_eq!(thing, "bruh.txt");
+*//*
+    let mut file1 = File::open(&thing as &str).expect("File not found");
+
+    let mut data : String = String::new();
+
+    file1.read_to_string(&mut data).expect("Something went wrong reading the file");
+
+    println!("Contents: {}", data);
+
+
 
 }
+*/
