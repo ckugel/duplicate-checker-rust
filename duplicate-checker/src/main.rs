4eed2a8a6db622ce2b9fbe3918b422c38d0178b8
Caleb Kugel <calebkugel1@gmail.com>
2022-07-15 16:32:54 -0500

    found a way to hash JPGs

--- duplicate-checker/src/main.rs
+++ duplicate-checker/src/main.rs
@@ -1,6 +1,8 @@
 use std::collections::hash_set::HashSet;
 use std::collections::hash_map::HashMap;
 
+use diffs::Diff;
+
 use sha2::Sha512;
 use sha2::Digest;
 
@@ -15,8 +17,11 @@ use std::fs;
 use glob::glob;
 use std::path::PathBuf;
 
-
 use std::io::BufReader;
+use img_hash::HasherConfig;
+
+extern crate image;
+extern crate img_hash;
 
 fn view_img(start : &str, extension: &str) -> () {
     let mut set : HashMap<Vec<u8>, Box<String>>  = HashMap::new();
@@ -53,19 +58,38 @@ fn view_img(start : &str, extension: &str) -> () {
 }
 
 fn main() -> std::io::Result<()> {
-    let mut start_folder : String = String::from("/mnt/3468843A6883F8BE/pictures-videos/vacations-holidays");
+    // let mut start_folder : String = String::from("/mnt/3468843A6883F8BE/pictures-videos/vacations-holidays");
 
     // println!("Enter a start folder: ");
     
     // stdin().read_line(&mut start_folder).expect("Failed to read line");
 
-    fs::canonicalize(&start_folder).ok();
+    // fs::canonicalize(&start_folder).ok();
 
     // start_folder.truncate(start_folder.len() - 1);
 
-    view_img(&start_folder as &str, "png");
-    view_img(&start_folder as &str, "JPG");
-    view_img(&start_folder as &str, "jpeg");
+    let path1: String = String::from("/mnt/3468843A6883F8BE/pictures-videos/vacations-holidays/San Francisco 2022/IMG_3224.JPG");
+    let path2: String = String::from("/mnt/3468843A6883F8BE/pictures-videos/vacations-holidays/Cali 2022/IMG_3224.JPG");
+
+    // let path1: String = String::from("folder1/Screenshot from 2021-09-02 22-25-30.png");
+    // let path2: String = String::from("folder1/Screenshot from 2021-10-17 23-22-40.png");
+
+    let image1 = image::open(path1).unwrap();
+    let image2 = image::open(path2).unwrap();
+
+    let hasher = HasherConfig::new().to_hasher();
+
+    let hash1 = hasher.hash_image(&image1);
+    let hash2 = hasher.hash_image(&image2);
+
+    println!("Image1 hash: {}", hash1.to_base64());
+    println!("Image2 hash: {}", hash2.to_base64());
+
+    println!("Hamming Distance: {}", hash1.dist(&hash2));
+
+    // view_img(&start_folder as &str, "png");
+    // view_img(&start_folder as &str, "JPG");
+    // view_img(&start_folder as &str, "jpeg");
     
 
     Ok(())
